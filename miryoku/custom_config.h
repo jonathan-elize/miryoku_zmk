2754995cbc81f989d2256b1c67b6fde6c0b732ee
Jonathan Elize <j-elize@ti.com>
2023-12-23 16:53:18 -0600

    Try to fix

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,16 +6,3 @@
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
-
-#define MIRYOKU_KLUDGE_GLOBALSHIFTFUNCTIONS
-
-// set-output "board" "nice_nano_v2"
-// set-output "shield" "corne_left,corne_right"
-// set-output "alphas" "QWERTY"
-// set-output "nav" "vi"
-// set-output "clipboard" "default"
-// set-output "layers" "default"
-// set-output "mapping" "default"
-// set-output "custom_config" "#define MIRYOKU_KLUDGE_THUMBCOMBOS\n#define MIRYOKU_KLUDGE_MOUSEKEYSPR"
-// set-output "kconfig" "CONFIG_ZMK_SLEEP=y"
-// set-output "branches" "zmkfirmware/zmk/main caksoylar/zmk/feat/mouse-keys-3.2"
