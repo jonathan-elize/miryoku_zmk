357997e39839faaf5ec3e012e494de56494d06be
Jonathan Elize <j-elize@ti.com>
2024-01-15 19:23:40 -0600

    fix: correct environment names

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,6 @@
-// Copyright 2021 Manna Harbour
+// Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_ALPHAS=QWERTY
-#define MIRYOKU_NAV=VI
-#define MIRYOKU_KLUDGE_THUMBCOMBOS=yes
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_NAV_VI
+#define MIRYOKU_KLUDGE_THUMBCOMBOS
